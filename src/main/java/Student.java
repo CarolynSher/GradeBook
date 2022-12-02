87762fcc1fab915e8026e993ae5e64ed9012a369
carolyn.sher <carolyn.sher@wgu.edu>
2022-12-02 15:47:46 -0500

    Initial commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,12 @@
+package PACKAGE_NAME;/**
+ * Project: GradeBook
+ * Package: PACKAGE_NAME
+ * <p>
+ * User: carolyn.sher
+ * Date: 12/2/2022
+ * Time: 3:33 PM
+ * <p>
+ * Created with IntelliJ IDEA
+ * To change this template use File | Settings | File Templates.
+ */public class Student {
+}
